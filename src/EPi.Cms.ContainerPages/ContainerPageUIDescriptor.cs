3a3dd7c07dd11077ac29314cb5674666f579682c
Jeroen Slor <jeroen.slor@gmail.com>
2016-02-13 01:35:46 +0100

    Initial version

---
+++ src/EPi.Cms.ContainerPages/ContainerPageUIDescriptor.cs
@@ -0,0 +1,17 @@
+using EPiServer.Shell;
+
+namespace EPi.Cms.ContainerPages
+{
+    /// <summary>
+    /// Describes how the UI should appear for <see cref="ContainerPage"/> content.
+    /// </summary>
+    [UIDescriptorRegistration]
+    public class ContainerPageUIDescriptor : UIDescriptor<IContainerPage>
+    {
+        public ContainerPageUIDescriptor()
+            : base(ContentTypeCssClassNames.Container)
+        {
+            DefaultView = CmsViewNames.AllPropertiesView;
+        }
+    }
+}
---
+++ src/EPi.Cms.ContainerPages/IContainerPage.cs
@@ -0,0 +1,9 @@
+﻿namespace EPi.Cms.ContainerPages
+{
+    /// <summary>
+    /// Marker interface for container pages
+    /// </summary>
+    public interface IContainerPage
+    {
+    }
+}
